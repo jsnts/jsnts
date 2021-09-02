7da3cc0d2643850398b245920d63e53740ba9c12
jsnts <jsantosg0703@outlook.com>
2021-09-02 10:04:05 -0500

    mate es cool v2.0

--- Computacion/tarea9.py
+++ Computacion/tarea9.py
@@ -15,7 +15,7 @@ def calculax1(a,b):
 def calculax2(a,b):
     return (-b - (discriminante ** 0.5)/2*a)
 
-print("\nBienvenido a la calculadora de ecuacionews cuadraticas")
+print("\nBienvenido a la calculadora de ecuaciones cuadraticas")
 
 print("\nTomando en cuenta que la ecuacion cuadratico tiene la forma")
 print(" Ax^2 + Bx + C = 0   siendo A!= 0")
@@ -27,3 +27,13 @@ c = float(input("C ==> "))
 
 discriminante = calcdicriminante(a,b,c)
 print("El valor de tu discriminante es de %5.2f" %(discriminante))
+
+if discriminante >= 0:
+    x1 = calculax1(a,b)
+    x2 = calculax2(a,b)
+    print("Las raices de la ecuacion son x1 = %7.2f    y     x2 = %7.2f " %(x1,x2))
+else:
+    print("\nLa ecuacion %5.2f   x^2  + %5.2f x + %5.2f = 0 no tiene raices reales " %(a,b,c))
+
+
+print("\nGracias por utilizar la calculadora de ecuaciones cuadraticas!!")
