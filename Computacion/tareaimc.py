a9ffcf9dd53093177e6c4a032a8681473225f38b
jsnts <jsantosg0703@outlook.com>
2021-09-20 09:35:57 -0500

    sdfghjklb

--- Computacion/tareaimc.py
+++ Computacion/tareaimc.py
@@ -1,28 +1,27 @@
 #Jair Santos A01026654
 #Ricardo Gaytán A00829872
 #Emilio Flores A01571179
-
-while cont == y:
+continua = "s"
+while continua != "n":
     #Saludo
-    print("Hola, espero que hoy estes bien!!")
+    print("\nHola, espero que hoy estes bien!!")
 
     #Ingresar datos
-    print("Me podrias decir tu peso?")
+    print("\nMe podrias decir tu peso?")
     peso = float(input())
-    print("Muchas gracias, ahora me puedes decir tu estatura en metros?")
+    print("\nMuchas gracias, ahora me puedes decir tu estatura en metros?")
     estatura = float(input())
     imc = peso/(estatura * estatura)
     #Resultados
     if (imc < 18.5):
-    print("Tu tienes una insuficiencia nutrimental deberias comer mejor y poner.")
+        print("\nTu tienes una insuficiencia nutrimental deberias comer mejor y poner.")
     elif (imc < 24.9):
-    print("Usted esta en buena condicion siga asi y procure no subir de peso.")
+        print("\nUsted esta en buena condicion siga asi y procure no subir de peso.")
     elif (imc < 29.9):
-    print("Usted esta en sobrepeso le recomiendo ponerse a dieta y empezar a ejercitarse un poco.")
+        print("\nUsted esta en sobrepeso le recomiendo ponerse a dieta y empezar a ejercitarse un poco.")
     elif (imc > 30.0):
-    print("Usted tiene obesidad lo recomendable es ponerse en una dieta para enflacar rapidamente y hacer ejercicio regularmente.")
+        print("\nUsted tiene obesidad lo recomendable es ponerse en una dieta para enflacar rapidamente y hacer ejercicio regularmente.")
         
-    print("Quiere medir su IMC otra vez s/n?")
-    input("")
-
+    print("\nQuiere medir su IMC otra vez s/n?")
+    continua = input("")
 
