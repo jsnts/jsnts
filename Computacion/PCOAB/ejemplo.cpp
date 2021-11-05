aca7aeff2bd8dc836ab9330a93957668dc593131
jsnts <jsantosg0703@outlook.com>
2021-11-05 12:09:46 -0600

    hgfaujdga

---
+++ Computacion/PCOAB/ejemplo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int calculaSuma(int a, int b)
+{
+    int suma=a+b;
+    return suma;
+}
+int main()
+{
+    int x, y;
+    cout<<"Entrar con x ="<<endl;
+    cin>>x;
+    cout<<"Entrar con y ="<<endl;
+    cin>>y;
+    cout<<"El resultado es = "<<x<<" + "<<y<<calculaSuma<<endl;
+}
