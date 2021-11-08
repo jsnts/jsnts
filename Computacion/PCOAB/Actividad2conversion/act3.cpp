d514250ccceddef7432f356d3c10018b17927bd1
jsnts <jsantosg0703@outlook.com>
2021-11-08 12:49:01 -0600

    safasfsas

---
+++ Computacion/PCOAB/Actividad2conversion/act3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int conversion(int cel){   
+    int far = (cel * (9/5)) + 32;
+    printf("%d", far, cel);
+}
+
+int main()
+{
+    int veces, cel;
+    cout<<"Cuantos valores quieres convertir: "<<endl;
+    cin>>veces;
+    int i;
+    for (i = 0; i <= veces; i++)
+        {
+            cout<<"Cual es el numero en celsius: "<<endl;
+            cin>>cel;
+            conversion(cel);
+        }
+}
