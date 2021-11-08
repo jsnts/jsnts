6946f77b43b3899848e2a954d926734f2381bed6
jsnts <jsantosg0703@outlook.com>
2021-11-08 12:48:56 -0600

    safasffa

--- Computacion/PCOAB/Actividad2/act2.cpp
+++ Computacion/PCOAB/Actividad2/act2.cpp
@@ -1,36 +1,33 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
-int y = 6;
+int magic(){
+    int count = 0;
+    int rando = rand() % 100+1;
+    while (true){
+        count += 1;
+        int guess;
+        cout << "Ingresa un numero entre 1 y 100: ";
+        cin >> guess;
+        if (guess > rando){
+            cout << "MENOS" << endl;
+        }else if (guess < rando){
+            cout << "MAS" <<endl;
+        }else if (guess == rando){
+            cout << "Lo Lograste!! Le atinaste al numero en "<< count << " intentos" << endl;
+            return 0;
+        }
+        
+    }
 
-int chooseRandoms(int lower, int upper, int count)
-{
-    int num = (rand() % (upper - lower +1)) + lower;
-    return num;
 }
 
-int main()
-{
-    
-    
-    int y;
-    int num = chooseRandoms(1, 100, 1);
-    while (num != y);
-        cout<<"\nIngresa un numero: "<<endl;
-        cin>>y;
-        if (y == num){
-            cout<<"\nEl numero "<<y<<" que elegiste fue el correcto."<<endl;
-        }
-        if (y < num){
-            cout<<"El numero que elegiste es menor al correcto, elige otra vez: "<<endl;
-            cin>>y;
-        }
-        if (y > num){
-            cout<<"El numero que elegiste es mayor al correcto, elige otra vez: "<<endl;
-            cin>>y;
-        }
-}
+int main(){
+    srand((unsigned) time(0));
+    int resp = magic();
 
+}
