637dbc9ff41ce2cde0acdf7e7be8961647293d64
jsnts <jsantosg0703@outlook.com>
2021-11-08 12:13:48 -0600

    gsdgdsg

--- Computacion/PCOAB/Actividad2/act2.cpp
+++ Computacion/PCOAB/Actividad2/act2.cpp
@@ -1,35 +1,36 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 using namespace std;
 
-void printRandoms(int lower, int upper, int count, int x)
+int y = 6;
+
+int chooseRandoms(int lower, int upper, int count)
 {
-    int i;
-    for (i=0; i < count; i++) {
-        int num = (rand() %
-            (upper - lower +1)) + lower;
-        x = num;
-    }
+    int num = (rand() % (upper - lower +1)) + lower;
+    return num;
 }
 
 int main()
 {
-    int x, y;
-    while (x != y);
-    cout<<"\nIngresa un numero: "<<endl;
-    cin>>y;
-    if (y == x){
-        cout<<"\nEl numero "<<y<<" que elegiste fue el correcto."<<endl;
-    }
-    else if (y < x){
-        cout<<"El numero que elegiste es menor al correcto, elige otra vez: "<<endl;
+    
+    
+    int y;
+    int num = chooseRandoms(1, 100, 1);
+    while (num != y);
+        cout<<"\nIngresa un numero: "<<endl;
         cin>>y;
-    }
-    else if (y > x){
-        cout<<"El numero que elegiste es mayor al correcto, elige otra vez: "<<endl;
-        cin>>y;
-    }
+        if (y == num){
+            cout<<"\nEl numero "<<y<<" que elegiste fue el correcto."<<endl;
+        }
+        if (y < num){
+            cout<<"El numero que elegiste es menor al correcto, elige otra vez: "<<endl;
+            cin>>y;
+        }
+        if (y > num){
+            cout<<"El numero que elegiste es mayor al correcto, elige otra vez: "<<endl;
+            cin>>y;
+        }
 }
+
