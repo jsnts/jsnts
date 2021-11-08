be5583d5715481de4cab60b2635bb02f142c1f6b
jsnts <jsantosg0703@outlook.com>
2021-11-08 13:08:45 -0600

    safasfa

--- Computacion/PCOAB/Actividad2conversion/act3.cpp
+++ Computacion/PCOAB/Actividad2conversion/act3.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int conversion(int cel){   
     int far = ((9/5) * cel) + 32;
-    printf("\nFarenheit: " + far);
-    printf("\nCelsius: " + cel);
+    printf("\nFarenheit: %i", far);
+    printf("\nCelsius: %i", cel);
     return 0;
 }
 
