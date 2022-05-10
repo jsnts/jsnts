372b35fd7ba63a121b1adeb350707db667904d7f
jsnts <jsantosg0703@outlook.com>
2022-05-10 12:04:29 -0500

    snake

--- Computacion/asfaf.py
+++ Computacion/asfaf.py
@@ -1,130 +1,70 @@
-import turtle
-import random
- 
-w = 500
-h = 500
-food_size = 10
-delay = 100
- 
-offsets = {
-    "up": (0, 20),
-    "down": (0, -20),
-    "left": (-20, 0),
-    "right": (20, 0)
-}
- 
-def reset():
-    global snake, snake_dir, food_position, pen
-    snake = [[0, 0], [0, 20], [0, 40], [0, 60], [0, 80]]
-    snake_dir = "up"
-    food_position = get_random_food_position()
-    food.goto(food_position)
-    move_snake()
-     
-def move_snake():
-    global snake_dir
- 
-    new_head = snake[-1].copy()
-    new_head[0] = snake[-1][0] + offsets[snake_dir][0]
-    new_head[1] = snake[-1][1] + offsets[snake_dir][1]
- 
-     
-    if new_head in snake[:-1]:
-        reset()
+"""Snake, classic arcade game.
+
+Exercises
+
+1. How do you make the snake faster or slower?
+2. How can you make the snake go around the edges?
+3. How would you move the food?
+4. Change the snake to respond to mouse clicks.
+"""
+
+from random import randrange
+from turtle import *
+
+from freegames import square, vector
+
+food = vector(0, 0)
+snake = [vector(10, 0)]
+aim = vector(0, -10)
+
+
+def change(x, y):
+    """Change snake direction."""
+    aim.x = x
+    aim.y = y
+
+
+def inside(head):
+    """Return True if head inside boundaries."""
+    return -200 < head.x < 190 and -200 < head.y < 190
+
+
+def move():
+    """Move snake forward one segment."""
+    head = snake[-1].copy()
+    head.move(aim)
+
+    if not inside(head) or head in snake:
+        square(head.x, head.y, 9, 'red')
+        update()
+        return
+
+    snake.append(head)
+
+    if head == food:
+        print('Snake:', len(snake))
+        food.x = randrange(-15, 15) * 10
+        food.y = randrange(-15, 15) * 10
     else:
-        snake.append(new_head)
- 
-     
-        if not food_collision():
-            snake.pop(0)
- 
- 
-        if snake[-1][0] > w / 2:
-            snake[-1][0] -= w
-        elif snake[-1][0] < - w / 2:
-            snake[-1][0] += w
-        elif snake[-1][1] > h / 2:
-            snake[-1][1] -= h
-        elif snake[-1][1] < -h / 2:
-            snake[-1][1] += h
- 
- 
-        pen.clearstamps()
- 
-         
-        for segment in snake:
-            pen.goto(segment[0], segment[1])
-            pen.stamp()
- 
-         
-        screen.update()
- 
-        turtle.ontimer(move_snake, delay)
- 
-def food_collision():
-    global food_position
-    if get_distance(snake[-1], food_position) < 20:
-        food_position = get_random_food_position()
-        food.goto(food_position)
-        return True
-    return False
- 
-def get_random_food_position():
-    x = random.randint(- w / 2 + food_size, w / 2 - food_size)
-    y = random.randint(- h / 2 + food_size, h / 2 - food_size)
-    return (x, y)
- 
-def get_distance(pos1, pos2):
-    x1, y1 = pos1
-    x2, y2 = pos2
-    distance = ((y2 - y1) ** 2 + (x2 - x1) ** 2) ** 0.5
-    return distance
-def go_up():
-    global snake_dir
-    if snake_dir != "down":
-        snake_dir = "up"
- 
-def go_right():
-    global snake_dir
-    if snake_dir != "left":
-        snake_dir = "right"
- 
-def go_down():
-    global snake_dir
-    if snake_dir!= "up":
-        snake_dir = "down"
- 
-def go_left():
-    global snake_dir
-    if snake_dir != "right":
-        snake_dir = "left"
- 
- 
-screen = turtle.Screen()
-screen.setup(w, h)
-screen.title("Snake")
-screen.bgcolor("blue")
-screen.setup(500, 500)
-screen.tracer(0)
- 
- 
-pen = turtle.Turtle("square")
-pen.penup()
- 
- 
-food = turtle.Turtle()
-food.shape("square")
-food.color("yellow")
-food.shapesize(food_size / 20)
-food.penup()
- 
- 
-screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_right, "Right")
-screen.onkey(go_down, "Down")
-screen.onkey(go_left, "Left")
- 
- 
-reset()
-turtle.done()
+        snake.pop(0)
+
+    clear()
+
+    for body in snake:
+        square(body.x, body.y, 9, 'black')
+
+    square(food.x, food.y, 9, 'green')
+    update()
+    ontimer(move, 100)
+
+
+setup(420, 420, 370, 0)
+hideturtle()
+tracer(False)
+listen()
+onkey(lambda: change(10, 0), 'Right')
+onkey(lambda: change(-10, 0), 'Left')
+onkey(lambda: change(0, 10), 'Up')
+onkey(lambda: change(0, -10), 'Down')
+move()
+done()
